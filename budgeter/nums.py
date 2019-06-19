b1af2851ea48807e13da0124bee705da559ea5d4
Kyle Mulleady <kyle@windwardapps.com>
2019-06-19 11:16:39 -1000

    python3

--- budgeter/nums.py
+++ budgeter/nums.py
@@ -12,6 +12,7 @@ from os.path import basename
 
 locale.setlocale(locale.LC_ALL, '')
 
+
 def crunch(filenames):
     data = []
     for filename in filenames:
@@ -27,6 +28,7 @@ def crunch(filenames):
                     })
     return sorted(data, key=lambda k: k['date'])
 
+
 def _get_filenames(date=None):
     if date is None:
         now = datetime.datetime.now()
@@ -38,6 +40,7 @@ def _get_filenames(date=None):
 
     month_year = '%04d_%02d' % (year, month)
     return glob.glob('data/*%s.csv' % month_year)
+
 
 def _get_reader(f):
     map = {
@@ -52,7 +55,8 @@ def _get_reader(f):
     reader = csv.DictReader(f, fieldnames)
     return (reader, negate_value)
 
-def _should_skip(row, negate_value, config={ 'flags': [] }):
+
+def _should_skip(row, negate_value, config={'flags': []}):
     try:
         amt = float(row['amount'])
         if row['amount'] == '':
@@ -68,6 +72,7 @@ def _should_skip(row, negate_value, config={ 'flags': [] }):
         return True
     return False
 
+
 def _print_results(results, min=0):
     total = 0
     gtotal = 0
@@ -76,18 +81,23 @@ def _print_results(results, min=0):
         gtotal += result['amt']
         if result['amt'] >= min:
             total += result['amt']
-            out.append('\t%s\t\t%s\t\t%s' % (locale.currency(result['amt']), result['date'], result['desc']))
+            out.append('\t%s\t\t%s\t\t%s' % (locale.currency(
+                result['amt']), result['date'], result['desc']))
     out.append('')
 
     if min > 0:
-        print '\n'.join(out)
-        print len(out) - 2, 'purchases over %s, sum %s' % (locale.currency(min), locale.currency(total))
-    print len(results), 'purchases total, sum', locale.currency(gtotal)
+        print('\n'.join(out))
+        print(len(out) - 2, 'purchases over %s, sum %s' %
+              (locale.currency(min), locale.currency(total)))
+    print(len(results), 'purchases total, sum', locale.currency(gtotal))
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-d', '--date', type=int, help='specify YYYYMM to process')
-    parser.add_argument('-m', '--min', type=int, help='specify min amount to show details')
+    parser.add_argument('-d', '--date', type=int,
+                        help='specify YYYYMM to process')
+    parser.add_argument('-m', '--min', type=int,
+                        help='specify min amount to show details')
     args = parser.parse_args()
     results = crunch(_get_filenames(args.date))
     _print_results(results, args.min)
