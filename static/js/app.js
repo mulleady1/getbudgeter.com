14cab24bdb2c78937d998237a5e66c8353459b79
Kyle Mulleady <kyle@windwardapps.com>
2025-07-24 21:41:01 -1000

    shoelace -> webawesome

--- static/js/app.js
+++ static/js/app.js
@@ -2,20 +2,18 @@ htmx.defineExtension("submit-spinner", {
   onEvent: function (name, event) {
     if (name === "htmx:load") {
       const parent = event.target
-      const elts = parent.querySelectorAll('button[type="submit"], sl-button[type="submit"]')
+      const elts = parent.querySelectorAll('button[type="submit"], wa-button[type="submit"]')
       if (elts.length > 0) {
         parent.addEventListener("htmx:beforeRequest", (event) => {
           elts.forEach((elt) => {
             elt.disabled = true
-            elt._innerHTML = elt.innerHTML
-            elt.innerHTML = "<sl-spinner style='--indicator-color: #eee; --track-color: #333;'></sl-spinner>"
+            elt.loading = true
           })
         })
         parent.addEventListener("htmx:afterRequest", (event) => {
           elts.forEach((elt) => {
             elt.disabled = false
-            elt.innerHTML = elt._innerHTML
-            delete elt._innerHTML
+            elt.loading = false
           })
         })
       }
@@ -33,30 +31,30 @@ function customConfirm(options) {
     let yes = false
     const div = document.createElement("div")
     div.innerHTML = `
-  <sl-dialog label="${title}" class="dialog-overview">
+  <wa-dialog label="${title}" class="dialog-overview">
     ${text}
     <div slot="footer">
-      <sl-button class="cancel">${cancelButtonText}</sl-button>
-      <sl-button class="confirm" variant="primary">${confirmButtonText}</sl-button>
+      <wa-button class="cancel">${cancelButtonText}</wa-button>
+      <wa-button class="confirm" variant="primary">${confirmButtonText}</wa-button>
     </div>
-  </sl-dialog>
+  </wa-dialog>
     `
 
     const dialog = div.firstElementChild
-    dialog.addEventListener("sl-after-hide", () => {
+    dialog.addEventListener("wa-after-hide", () => {
       resolve(yes)
       div.remove()
     })
-    const closeButton = dialog.querySelector("sl-button.cancel")
-    const confirmButton = dialog.querySelector("sl-button.confirm")
-    closeButton.addEventListener("click", () => dialog.hide())
+    const closeButton = dialog.querySelector("wa-button.cancel")
+    const confirmButton = dialog.querySelector("wa-button.confirm")
+    closeButton.addEventListener("click", () => (dialog.open = false))
     confirmButton.addEventListener("click", () => {
       yes = true
-      dialog.hide()
+      dialog.open = false
     })
     document.body.appendChild(div)
     setTimeout(() => {
-      dialog.show()
+      dialog.open = true
     }, 100)
   })
 }
